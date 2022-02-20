08b6d1f87ec8d270d23e19a13bb5b6dbb58404f4
Alexander Shakhov <xander.shahov@gmail.com>
2022-02-20 23:49:54 +0300

    home page markup is done

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 `npx create-react-app <app_name>`
 
 `npm start`
+
+### References
+
+1. CSS Grid Layout:
+    https://www.w3schools.com/css/css_grid.asp
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-wrapper">
+      <header className="header">
+        Header
       </header>
+      <div className="navbar">
+        Navbar
+      </div>
+      <div className="content">
+        Content
+      </div>
+      <div className="footer">
+        Footer
+      </div>
     </div>
   );
 }
